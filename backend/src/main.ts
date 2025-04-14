e24a51150dc67845d7727af44fa4906254371f6c
Anandhu Prakash <anandhuprakash682@gmail.com>
2025-04-14 09:48:17 +0530

    swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('FintrackPro API')
+    .setDescription(
+      'Provides a comprehensive API for managing personal finances, including income, expenses, budgets, and goals.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'refresh-token',
+    )
+
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
